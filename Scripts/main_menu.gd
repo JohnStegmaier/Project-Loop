8e85570e43b36a06680b843849387054125bdaca
John Stegmaier <jupdown@protonmail.com>
2025-07-30 17:47:17 -0400

    add main menu, options menu and first level of the game with logic to switch between each

---
+++ Scripts/main_menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Game Scenes/01_loop_start.tscn")
+	pass # Replace with function body.
+
+
+func _on_options_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Game Scenes/00_options_menu.tscn")
+	pass # Replace with function body.
+
+
+func _on_quit_game_pressed() -> void: # Quit the game
+	get_tree().quit()
+	pass # Replace with function body.
