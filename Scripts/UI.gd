efce55372b13cf6ca8821bee3052d2e9422b669a
John Stegmaier <jupdown@protonmail.com>
2025-08-01 18:32:05 -0400

    Add battery bar to UI on all scenes and logic to decrement it over time

--- Scripts/UI.gd
+++ Scripts/UI.gd
@@ -1,7 +1,17 @@
 extends CanvasLayer
 
+const DEBUG_LOG = true
+
 @onready var score_label: RichTextLabel = get_node("Control/Panel/Score")
+@onready var battery_bar: ProgressBar = get_node("Control/LifeBarPanel/Battery")
 
 func _ready() -> void:
 	ScoreManager.score_label = get_node("Control/Panel/Score")
 	score_label.text = "[center] Score: [color=yellow]%d[/color][/center]" % ScoreManager.score
+	battery_bar.value = ScoreManager.health
+	
+func _process(delta: float) -> void:
+	ScoreManager.health -= 0.00001
+	battery_bar.value = ScoreManager.health
+	Logger.log_debug("Battery bar value: %s" % battery_bar.value, DEBUG_LOG)
+	Logger.log_debug("ScoreManager.health value: %s" % ScoreManager.health, DEBUG_LOG)
