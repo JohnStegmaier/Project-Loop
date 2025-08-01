0d23d59345b0f78cc597af8c42ddfcacb1a4aff4
John Stegmaier <jupdown@protonmail.com>
2025-08-01 17:54:52 -0400

    Fix horrid static referencing and provide proper type assignments

--- Scripts/UI.gd
+++ Scripts/UI.gd
@@ -1,7 +1,7 @@
 extends CanvasLayer
 
-@onready var score_label = $Control/Panel/Score
+@onready var score_label: RichTextLabel = get_node("Control/Panel/Score")
 
 func _ready() -> void:
-	ScoreManager.score_label = $Control/Panel/Score
+	ScoreManager.score_label = get_node("Control/Panel/Score")
 	score_label.text = "[center] Score: [color=yellow]%d[/color][/center]" % ScoreManager.score
