67288b2cc62af499af496fce94049bf6dd9fa0e4
John Stegmaier <jupdown@protonmail.com>
2025-08-02 17:54:14 -0400

    Update the way objects take damage to scale based on the amount the blaster shot is charged.

--- Scripts/destructible_object.gd
+++ Scripts/destructible_object.gd
@@ -1,16 +1,16 @@
 extends Area2D
 
-@export var hit_damage : int = 20
-@export var health : int = 100
-const DEBUG_LOG = false;
+@export var hit_damage : float = 20
+@export var health : float = 100
+const DEBUG_LOG = true;
 
 func _ready() -> void:
 	connect("body_entered", Callable(self, "_on_body_entered"))
 	pass # Replace with function body.
 
-func take_hit():
-	health -= hit_damage
-	Logger.log_debug("%s object took a hit dealing %s damage. Remainnig health %s" % [self.name, hit_damage, health], DEBUG_LOG)
+func take_hit(damage_scale : float = 1):
+	health -= hit_damage * damage_scale
+	Logger.log_debug("%s object took a hit dealing (%s×%s) damage. Remainnig health %s" % [self.name, hit_damage, damage_scale, health], DEBUG_LOG)
 	check_for_destruction()
 
 func check_for_destruction() -> void:
