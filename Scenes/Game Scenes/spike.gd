35d79c0146f1acd9ae7e205863bc947911990356
ChronoFrost115 <lowramadvantage@gmail.com>
2025-08-03 00:26:20 -0400

    Spike script cleanup
    duplicate spike script removed. Correct one added to scripts folder.

--- Scenes/Game Scenes/spike.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-func _ready():
-	body_entered.connect(_on_body_entered)
-	
-func _on_body_entered(body):
-	if body is CharacterBody2D and body.name == "Player":
-		print("battery should reduce. code this.")
-		get_tree().reload_current_scene()
--- Scripts/spike.gd
+++ Scripts/spike.gd
@@ -1,6 +1,9 @@
-extends Node2D
+extends Area2D
 
+func _ready():
+	body_entered.connect(_on_body_entered)
+	
 func _on_body_entered(body):
 	if body is CharacterBody2D and body.name == "Player":
-		get_tree().reload_current_scene()
 		print("battery should reduce. code this.")
+		get_tree().reload_current_scene()
